544949baad487cca6836ce6ac44a908c3cdea80a
Jodie Mullins <jmullins2460@gmail.com>
2023-08-19 20:11:42 -0400

    create, write Aug 19 pd df

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
-# add your code here
+import pandas as pd
+
+fruit_sales = pd.DataFrame(columns=["Apples", "Bananas"])
+
+fruit_sales = pd.DataFrame({'Apples': [35, 41],
+                           'Bananas' : [21, 34]},
+                          index=['2017 Sales', '2018 Sales'])
+
+write_fruit_sales = fruit_sales.to_csv(r'C:\Users\jomuf\Documents\CODE KENTUCKY PROJECTS\DLGITHF\fruit-sales-exercise-JodieMullins\fruit.csv')
